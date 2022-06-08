9c1940291c0809e9686c84c945cb51334b364935
error by night <abinv321@gmail.com>
2022-06-08 14:54:22 +0530

    biggest of four no

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+<<<<<<< Updated upstream
 function functionNwqś(params) {
   console.log("gww");
+=======
+let a = 98,
+  b = 44,
+  c = 38,
+  d = 33;
+if (a > b)
+  if (a > c)
+    if (a > d) {
+      alert("a is big");
+    } else {
+      alert("d is big");
+    }
+  else if (c > d) {
+    alert("c is big");
+  } else {
+    alert("d is big");
+  }
+else if (b > c)
+  if (b > d) {
+    alert("b is big");
+  } else {
+    alert("d is big");
+  }
+else if (c > d) {
+  alert("c is big");
+} else {
+  alert("d is big");
+>>>>>>> Stashed changes
 }
